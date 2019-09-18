b721d9863dd5d0c115be602ffb62326f0da29221
carlosmalaquias <carlosomalaquias@gmail.com>
2019-09-17 21:40:35 -0300

    User Insert

--- src/main/java/com/educandoweb/curso/services/UserService.java
+++ src/main/java/com/educandoweb/curso/services/UserService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.educandoweb.curso.dto.UserDTO;
+import com.educandoweb.curso.dto.UserInsertDTO;
 import com.educandoweb.curso.entities.User;
 import com.educandoweb.curso.repositories.UserRepository;
 import com.educandoweb.curso.services.excceptions.DatabaseExcception;
@@ -35,8 +36,10 @@ public class UserService {
 		return new UserDTO(entity);
 	}
 	
-	public User insert(User obj) {
-		return repository.save(obj);
+	public UserDTO insert(UserInsertDTO dto) {
+		User entity = dto.toEntity();
+		entity =  repository.save(entity);
+		return new UserDTO(entity);
 	}
 	
 	public void delete(Long id) {
