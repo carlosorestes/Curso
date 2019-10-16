5c214c7192eeaaf8aa6070056d4e8210564ba661
carlosmalaquias <carlosomalaquias@gmail.com>
2019-10-16 05:57:20 -0300

    Pre-authorizing requests

--- src/main/java/com/educandoweb/curso/resources/CategoryResource.java
+++ src/main/java/com/educandoweb/curso/resources/CategoryResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.curso.dto.CategoryDTO;
-import com.educandoweb.curso.entities.Category;
-import com.educandoweb.curso.entities.User;
 import com.educandoweb.curso.services.CategoryService;
 
 @RestController
@@ -39,6 +38,7 @@ public class CategoryResource {
 		return ResponseEntity.ok().body(dto);
 	}	
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PostMapping
 	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
 		CategoryDTO newDto = categoryService.insert(dto);
@@ -46,12 +46,14 @@ public class CategoryResource {
 		return ResponseEntity.created(uri).body(newDto);
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@DeleteMapping(value  = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		categoryService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PutMapping(value  = "/{id}")
 	public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto){
 		dto = categoryService.update(id, dto);
