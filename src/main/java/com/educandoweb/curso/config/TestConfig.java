4f71773ad67236cf8607fbf6f0fb67fe4de5b7d3
carlosmalaquias <carlosomalaquias@gmail.com>
2019-10-15 23:59:13 -0300

    Encoding user password

--- src/main/java/com/educandoweb/curso/config/TestConfig.java
+++ src/main/java/com/educandoweb/curso/config/TestConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.educandoweb.curso.entities.Category;
 import com.educandoweb.curso.entities.Order;
@@ -26,6 +27,9 @@ import com.educandoweb.curso.repositories.UserRepository;
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncode;
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -48,8 +52,8 @@ public class TestConfig implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", passwordEncode.encode("123456"));
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777",  passwordEncode.encode("123456"));
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		
